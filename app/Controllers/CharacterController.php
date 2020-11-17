bbc9aedd1405b3a1897038b54a61392ab8e6c35a
Sylanael <sylanael@gmail.com>
2020-11-17 19:15:15 +0800

    Sheet Updated
    Random Basic Melee Attack
    Radom Basic Ranged Attack
    Random Racial Power

--- app/Controllers/CharacterController.php
+++ app/Controllers/CharacterController.php
@@ -26,13 +26,16 @@ class CharacterController extends BaseController
 		$featsArray = json_decode(file_get_contents(base_url(). "/assets/json/feats.json"), true);
 		$raceFeatureArray = json_decode(file_get_contents(base_url(). "/assets/json/raceFeatures.json"), true);
 		$classTalentArray = json_decode(file_get_contents(base_url(). "/assets/json/classTalents.json"), true);
+		$weaponArray = json_decode(file_get_contents(base_url(). "/assets/json/weapons.json"), true);
 
 		$name = 'Character Name';
 		$player = 'Player Name';
 		$randomRace = array_rand($raceArray, 1);
-		$race = $raceArray[$randomRace]['name'];
+		$raceName = $raceArray[$randomRace]['name'];
+		$race = $raceArray[$randomRace];
 		$randomClass = array_rand($classArray, 1);
-		$class = $classArray[$randomClass]['name'];
+		$className = $classArray[$randomClass]['name'];
+		$class = $classArray[$randomClass];
 
 		#Choose Race Bonus 1 or 2
 		$raceBonusRandom = rand(1,2); // 1 = Bonus 1, 2 = Bonus 2
@@ -54,6 +57,10 @@ class CharacterController extends BaseController
 				$classBonus = $classArray[$randomClass]['bonus1'];
 			}
 		}
+
+		#Get the Racial Power
+		$racialPower = $raceArray[$randomRace]['racialpower'];
+
 
 		#Add Class Bonus
 		switch ($classBonus) {
@@ -99,6 +106,50 @@ class CharacterController extends BaseController
 			break;
 		}
 
+		#Count Weapon Amount
+		$meleeWeaponsCount = count($classArray[$randomClass]['meleeWeapons']);
+		$meleeWeaponsCountMax = $meleeWeaponsCount-1;
+		$meleeWeaponRandom = rand(0, $meleeWeaponsCountMax);
+		$meleeWeaponRandomName = $classArray[$randomClass]['meleeWeapons'];
+
+		$rangedWeaponsCount = count($classArray[$randomClass]['rangedWeapons']);
+		$rangedWeaponsCountMax = $rangedWeaponsCount-1;
+		$rangedWeaponRandom = rand(0, $rangedWeaponsCountMax);
+		$rangedWeaponRandomName = $classArray[$randomClass]['rangedWeapons'];
+
+		$x = 0;
+		foreach ($meleeWeaponRandomName as $key) {
+			if ($x <= $meleeWeaponRandom) {
+				$meleeWeaponName = $key;
+			}
+			$x++;
+		}
+
+		$y = 0;
+		foreach ($rangedWeaponRandomName as $key) {
+			if ($y <= $rangedWeaponRandom) {
+				$rangedWeaponName = $key;
+			}
+			$y++;
+		}
+
+		$meleeWeaponsArray = array(
+			"name" => $weaponArray['meleeWeapons'][$meleeWeaponName]['name'],
+			"type" => $weaponArray['meleeWeapons'][$meleeWeaponName]['type'],
+			"class" => $weaponArray['meleeWeapons'][$meleeWeaponName]['class'],
+			"damage" => $weaponArray['meleeWeapons'][$meleeWeaponName]['damage'],
+			"penalty" => $weaponArray['meleeWeapons'][$meleeWeaponName]['penalty'],
+		);
+
+		$rangedWeaponsArray = array(
+			"name" => $weaponArray['rangedWeapons'][$rangedWeaponName]['name'],
+			"type" => $weaponArray['rangedWeapons'][$rangedWeaponName]['type'],
+			"class" => $weaponArray['rangedWeapons'][$rangedWeaponName]['class'],
+			"damage" => $weaponArray['rangedWeapons'][$rangedWeaponName]['damage'],
+			"penalty" => $weaponArray['rangedWeapons'][$rangedWeaponName]['penalty'],
+		);
+
+
 		$strMod = floor(($str - 10)/2);
 		$dexMod = floor(($dex - 10)/2);
 		$conMod = floor(($con - 10)/2);
@@ -108,7 +159,6 @@ class CharacterController extends BaseController
 
 		$level = 1;
 		$hitPoints = 0;
-		$staggered = floor($hitPoints/2);
 		$armorClass = 0;
 		$physicalDef  = 0;
 		$mentalDef = 0;
@@ -117,12 +167,27 @@ class CharacterController extends BaseController
 		$recoveryDice = 0;
 
 		$hitPoints = ($classArray[$randomClass]['level1']['hitpoints-formula-1']+$conMod)*$classArray[$randomClass]['level1']['hitpoints-formula-2'];
+		$staggered = floor($hitPoints/2);
 		$totalAdventureFeats = $classArray[$randomClass]['level1']['totalAdventureFeats'];
 		$totalChampionFeats = $classArray[$randomClass]['level1']['totalChampionFeats'];
 		$totalEpicFeats = $classArray[$randomClass]['level1']['totalEpicFeats'];
 		$totalAdventureClassTalent = $classArray[$randomClass]['level1']['totalAdventureClassTalent'];
 		$totalChampionClassTalent = $classArray[$randomClass]['level1']['totalChampionClassTalent'];
 		$totalEpicClassTalent = $classArray[$randomClass]['level1']['totalEpicClassTalent'];
+
+
+		$racialPowerArray = array(
+			'name' => $raceFeatureArray[$racialPower]['name'],
+			'trigger' => $raceFeatureArray[$racialPower]['trigger'],
+			'effect' => $raceFeatureArray[$racialPower]['effect'],
+			'adventureFeat' => $raceFeatureArray[$racialPower]['adventureFeat'],
+			'championFeat' => $raceFeatureArray[$racialPower]['championFeat'],
+			'epicFeat' => $raceFeatureArray[$racialPower]['epicFeat'],
+		);
+
+		//Derived Stats
+		$strPlusLevel = $strMod + $level;
+		$dexPlusLevel = $dexMod + $level;
 
 
 		$output = array(
@@ -140,8 +205,8 @@ class CharacterController extends BaseController
 			'charismaModifier' => $chaMod, 
 			'name' => $name, 
 			'player' => $player, 
-			'race' => $race, 
-			'class' => $class, 
+			'race' => $raceName, 
+			'class' => $className, 
 			'raceBonus' => $raceBonus,
 			'classBonus' => $classBonus,
 			'level' => $level,
@@ -152,13 +217,18 @@ class CharacterController extends BaseController
 			'totalChampionClassTalent' => $totalChampionClassTalent,
 			'totalEpicClassTalent' => $totalEpicClassTalent,
 			'hitPoints' => $hitPoints,
-			'staggered' => $staggered,
 			'armorClass' => $armorClass,
+			'staggered' => $staggered,
 			'physicalDef' => $physicalDef,
 			'mentalDef' => $mentalDef,
 			'intiativeBonus' => $intiativeBonus,
 			'recoveries' => $recoveries,
 			'recoveryDice' => $recoveryDice,
+			'racialPower' => $racialPowerArray,
+			'rangedWeapon' => $rangedWeaponsArray,
+			'meleeWeapon' => $meleeWeaponsArray,
+			'strPlusLevel' => $strPlusLevel,
+			'dexPlusLevel' => $dexPlusLevel,
 		);
 
 		return view('sheet', $output);
--- app/Views/sheet.php
+++ app/Views/sheet.php
@@ -94,11 +94,11 @@
 
   <div class="padding-row-quater"></div>
   <div class="row">
-  	<div class="col-3">
+  	<div class="col-4">
   		<span class="levelBonus">Level: <?= $level ?></span>
       <span class="levelBonus">Initiative: <?= $intiativeBonus ?></span>
   	</div>
-  	<div class="col-5">
+  	<div class="col-4">
   		<span class="levelBonus">Recoveries: <?= $recoveries ?> + <?= $constitutionModifier ?></span>
   	</div>
   	<div class="col-4">
@@ -108,6 +108,189 @@
   	</div>
   </div>
   
+  <div class="padding-row-quater"></div>
+  
+  <div class="row">
+        <div class="col-4 collumSeperator">
+          
+            <div class="row marginauto">
+              <h2 class="h2-overwrite">One Unique Thing</h2>
+            </div>
+            <div class="row marginauto">
+              <textarea name="" rows="3" cols="40" class="textareaBackgrounds"> </textarea>
+            </div>
+
+            <div class="row marginauto">
+              <h2 class="h2-overwrite">Icon Relationship</h2>
+            </div>
+            <div class="row marginauto">
+              <textarea name="" rows="3" cols="40" class="textareaBackgrounds"> </textarea>
+            </div>
+      
+
+            <div class="row marginauto">
+              <h2 class="h2-overwrite">Backgrounds</h2>
+            </div>
+            <div class="row marginauto">
+              <textarea name="" rows="3" cols="40" class="textareaBackgrounds"> </textarea>
+            </div>
+
+             <div class="padding-row-quater"></div>
+
+
+              <div class="row marginauto">
+              <h2 class="h2-overwrite">Talents & Features</h2>
+              </div>
+              <div class="row borderBox">
+                <p>
+                  <strong>Building Frenzy: </strong>
+                  You gain the Building Frenzy ability, usable one battle per day.
+                </p>
+                  
+                <p>
+                  <strong>Slayer: </strong>
+                  Whenever you attack a staggered enemy that you were not engaged with at the state of your turn, you deal +2d6 damage to that creature if you hit.
+                </p>
+
+                <p>
+                  <strong>Barbaric Cleave: </strong>
+                  Once per battle as a free action, make another melee attack after you have dropped a non book foe to 0 health. Dropping the last mook of a book mob also qualifies to use Barbaric Cleave.
+                </p>
+              </div>
+
+        </div>
+
+        <div class="col-4 collumSeperator">
+
+          <div class="row featsBox">
+            <div class="col-7">
+            <strong><?= $meleeWeapon['name'] ?></strong> <?= $meleeWeapon['type'] ?>
+            </div>
+            <div class="col-5">
+            <strong>At-Will</strong>
+            </div>
+            <div class="col-7">
+            Standard Action  
+            </div>
+            <div class="col-5">
+            Melee Basic Attack
+            </div>
+            <div class="col-7">
+            <strong>Target: </strong>One creature 
+            </div>
+            <div class="col-5">
+            <strong>Attack: </strong>+<?= $strPlusLevel ?> vs AC
+            </div>
+            <div class="col-12">
+            <strong>Hit: </strong>  
+            <?= $level.$meleeWeapon['damage'] ?> + <?= $strengthModifier ?> 
+            </div>
+            <div class="col-12">
+            <strong>Miss: </strong>Damage equal to level (<?= $level ?>)
+            </div>
+          </div>
+
+          <div class="row featsBox">
+            <div class="col-7">
+            <strong><?= $rangedWeapon['name'] ?></strong> <?= $rangedWeapon['type'] ?>
+            </div>
+            <div class="col-5">
+            <strong>At-Will</strong>
+            </div>
+            <div class="col-7">
+            Standard Action  
+            </div>
+            <div class="col-5">
+            Ranged Basic Attack
+            </div>
+            <div class="col-7">
+            <strong>Target: </strong>One creature 
+            </div>
+            <div class="col-5">
+            <strong>Attack: </strong>+<?= $strPlusLevel ?> vs AC
+            </div>
+            <div class="col-12">
+            <strong>Hit: </strong>  
+            <?= $level.$rangedWeapon['damage'] ?> + <?= $dexterityModifier ?> 
+            </div>
+            <div class="col-12">
+            <strong>Miss: </strong>Damage equal to level (<?= $level ?>)
+            </div>
+          </div>
+
+          <div class="row featsBox">
+            <div class="col-7">
+            <strong>Barbaric Cleave</strong>
+            </div>
+            <div class="col-5">
+            <strong>Barbarian</strong>
+            </div>
+            <div class="col-12">
+            <strong>Trigger: </strong>You drop a foe to 0 hp with a barbarian melee attack.
+            </div>
+            <div class="col-12">
+            <strong>Effect: </strong>Make another barbarian melee attack against another target.
+            </div>
+          </div>
+          
+          <div class="row featsBox">
+            <div class="col-7">
+            <strong><?= $racialPower['name'] ?></strong>
+            </div>
+            <div class="col-5">
+            <strong><?= $race ?></strong>
+            </div>
+            <div class="col-12">
+            <strong>Trigger: </strong><?= $racialPower['trigger'] ?>
+            </div>
+            <div class="col-12">
+            <strong>Effect: </strong><?= $racialPower['effect'] ?>
+            </div>
+          </div>
+
+        </div>
+
+        <div class="col-4 collumSeperator">
+
+          <div class="row featsBox">
+            <div class="col-7">
+            <strong>Building Frenzy</strong>
+            </div>
+            <div class="col-5">
+            <strong>Daily</strong>
+            </div>
+            <div class="col-12">
+            <strong>Trigger: </strong>You miss with an attack.
+            </div>
+            <div class="col-12">
+            <strong>Effect: </strong>You deal +1d6 damage with melee attacks until the end of the battle. Increase the additional damage by +1d6 each time one of your attack misses, up to a maximum of +4d6.
+            </div>
+            <div class="col-12">
+            <strong>Adventurer Feat: </strong>You deal +1d6 damage with melee attacks until the end of the battle. Increase the additional damage by +1d6 each time one of your attack misses, up to a maximum of +4d6.
+            </div>
+          </div>
+
+           <div class="row featsBox">
+            <div class="col-7">
+            <strong>Barbarian Rage</strong>
+            </div>
+            <div class="col-5">
+            <strong>Recharge 13+</strong>
+            </div>
+            <div class="col-12">
+              <strong>Free Action</strong>
+            </div>
+            <div class="col-12">
+            <strong>Effect: </strong>You fling into a rage for the rest of the battle.  While raging, you roll 2d20 to hit with your melee and thrown weapon attacks instead of 1d20. Use the higher roll for the attack. If  you roll a natural 11+ with both dice and your best attack roll is it a hit, the attack is automatically a critical hit!
+            </div>
+            <div class="col-12">
+            <strong>Adventurer Feat: </strong>Whenever the escalation die is 4+, as a quick action, you can start raging for free (it doesn't count as your normal usage and does not need a recharge)
+            </div>
+          </div>
+
+        </div>
+  </div>
+
   	</div>
     <!-- JavaScript -->
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
